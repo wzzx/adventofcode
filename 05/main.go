28f2ccb1780f7e8f5868c870a417ad4a33836ac9
wzzx <wzzx@rmo.so>
2018-12-05 22:21:29 +0100

    1st part working solution
    Wasted a lot of time here because I was using the resulting polymer as
    the solution. Should have read that the solution is the length of the
    polymer

--- 05/main.go
+++ 05/main.go
@@ -42,23 +42,25 @@ func main() {
 	for i := 1; i < len(polymer); i++ {
 		reaction := reactPair(polymer[i-1], polymer[i])
 		if reaction {
-			//fmt.Printf("\t[%d] There's a reaction!: '%s' '%s'\n", i, string(polymer[i-1]), string(polymer[i]))
 			if i+1 < len(polymer) {
 				polymer_right = polymer[i+1:]
 			} else {
-				break
+				polymer_right = ""
 			}
 
 			polymer = polymer_left + polymer_right
 			// make sure we don't miss the first character
 			polymer_left = ""
 			polymer_right = ""
-			i = 0
-		} else {
-			polymer_left += string(polymer[i-1])
+			// -2 because it will be incremented in the for loop
+			if i-2 < 0 {
+				i = 0
+			} else {
+				i -= 2
+			}
 		}
-		//fmt.Printf("Polymer orig: %s\nPolymer: %s\n\tPolymer left: %s\n\tPolymer right: %s\n", polymer_orig, polymer, polymer_left, polymer_right)
+		polymer_left = string(polymer[:i])
 	}
 
-	fmt.Printf("Resulting polymer: %s\n", polymer)
+	fmt.Printf("Polymer: %s\nUnits: %d\n", polymer, len(polymer))
 }
